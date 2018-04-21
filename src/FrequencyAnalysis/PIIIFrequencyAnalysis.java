22e91aab7d9beb22bcccdec5f443a4747293a930
amos_zhao <amos_zhao@163.com>
2018-04-21 12:40:56 +0800

    v1.0 pIII finish

--- src/FrequencyAnalysis/PIIIFrequencyAnalysis.java
+++ src/FrequencyAnalysis/PIIIFrequencyAnalysis.java
@@ -1,7 +1,9 @@
 package FrequencyAnalysis;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @function do frequency analysis with p-III distribute
@@ -14,20 +16,17 @@ public class PIIIFrequencyAnalysis {
     private double ex;
     private double cs;
     private double cv;
-    private double csv;
+    private double csv=2;
 
-    private int N1;
-    private int N2;
-    private int l1;
-    private int l2;
+    /**
+     * Important!!!
+     * the unit of  frequency is %
+     * which means 0.999 in series is 99.9
+     */
+    private List<Double> frequencies=new ArrayList<>();
+    private List<Double> values=new ArrayList<>();
+    private List<Double> plotFrequencies=new ArrayList<>();
 
-    private List<Double> values;
-    private List<Double> frequencies;
-
-    private List<Double> plotValues;
-    private List<Double> plotFrequencies;
-
-    private Map<String ,List<Double>> map;
     public double getEx() {
         return ex;
     }
@@ -65,64 +64,15 @@ public class PIIIFrequencyAnalysis {
         return values;
     }
 
-    public void setData(List<Double> values) {
-        this.values = values;
-    }
-
-    public int getN1() {
-        return N1;
-    }
-
-    public void setN1(int n1) {
-        N1 = n1;
-    }
-
-    public int getN2() {
-        return N2;
-    }
-
-    public void setN2(int n2) {
-        N2 = n2;
-    }
-
-    public int getL1() {
-        return l1;
-    }
-
-    public void setL1(int l1) {
-        this.l1 = l1;
-    }
-
-    public int getL2() {
-        return l2;
-    }
-
-    public void setL2(int l2) {
-        this.l2 = l2;
-    }
-
-    public void setValues(List<Double> values) {
-        this.values = values;
-    }
-
     public List<Double> getFrequencies() {
         return frequencies;
     }
 
-    public void setFrequencies(List<Double> frequencies) {
-        this.frequencies = frequencies;
-    }
-
     public List<Double> getPlotValues() {
-        return plotValues;
-    }
-
-    public void setPlotValues(List<Double> plotValues) {
-        this.plotValues = plotValues;
+        return FrequencyDictionary.listSimulatePoints(plotFrequencies, ex, cs, cv);
     }
 
     public List<Double> getPlotFrequencies() {
-        plotFrequencies=FrequencyDictionary.getPlotFrequencies();
         return plotFrequencies;
     }
 
@@ -130,20 +80,12 @@ public class PIIIFrequencyAnalysis {
         this.plotFrequencies = plotFrequencies;
     }
 
-    public Map<String, List<Double>> getMap() {
-        return map;
-    }
-
-    public void setMap(Map<String, List<Double>> map) {
-        this.map = map;
-    }
-
-    public PIIIFrequencyAnalysis(double ex, double cs, double cv) {
+    PIIIFrequencyAnalysis(double ex, double cv, double cs) {
         this.ex = ex;
         this.cs = cs;
         this.cv = cv;
     }
-    public PIIIFrequencyAnalysis(double ex, double cv,double csv,int id) {
+    PIIIFrequencyAnalysis(double ex, double cv) {
         this.ex = ex;
         this.cv = cv;
         setCsv(csv);
@@ -153,10 +95,16 @@ public class PIIIFrequencyAnalysis {
      * there is no extrem value in data series
      * @param values series
      */
-    public PIIIFrequencyAnalysis(List<Double> values) {
+    PIIIFrequencyAnalysis(List<Double> values) {
+        this.setPlotFrequencies(FrequencyDictionary.getPlotFrequencies());
         this.values = values;
-        //TODO 矩法计算参数
-        //TODO 计算经验点距
+        this.values.sort(Collections.reverseOrder());
+        frequencies=new ArrayList<>();
+        calculateParameter();
+        for(int i=1;i<=values.size();i++){
+            frequencies.add(new BigDecimal((i/(values.size()+1.0))*100)
+                    .setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        }
     }
 
     /**
@@ -164,14 +112,24 @@ public class PIIIFrequencyAnalysis {
      * one return period
      * @param values data series
      * @param N1 first return period length
+     * @param a1 the count of first series
      * @param l1 the value which be count in first series who belongs to ordinary period
      */
-    public PIIIFrequencyAnalysis(List<Double> values,int N1,int l1) {
+    PIIIFrequencyAnalysis(List<Double> values, int N1, int a1, int l1) {
+        this.setPlotFrequencies(FrequencyDictionary.getPlotFrequencies());
         this.values = values;
-        this.N1=N1;
-        this.l1=l1;
-        //TODO 矩法计算参数
-        //TODO 计算经验点距 一个重现期
+        this.values.sort(Collections.reverseOrder());
+        frequencies=new ArrayList<>();
+        calculateParameter();
+        for(int i=1;i<=a1;i++){
+            frequencies.add(new BigDecimal((i/(N1+1.0))*100)
+                    .setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        }
+        double tmp=frequencies.get(a1-1)/100;
+        for(int i=l1+1;i<=values.size()-a1;i++){
+            frequencies.add(new BigDecimal((tmp+(1-tmp)* i/(values.size()-a1+1.0))*100)
+                    .setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        }
     }
 
     /**
@@ -179,18 +137,32 @@ public class PIIIFrequencyAnalysis {
      * two return period
      * @param values data series
      * @param N1 first return period length
+     * @param a1 the data size of first series
      * @param l1 the value which be count in first series who belongs to second period
      * @param N2 second return period length
+     * @param a2 the data size of second series
      * @param l2 the value which be count in second  series who belongs to ordinary period
      */
-    public PIIIFrequencyAnalysis(List<Double> values,int N1,int l1,int N2,int l2) {
+    PIIIFrequencyAnalysis(List<Double> values, int N1, int a1, int l1, int N2, int a2, int l2) {
+        this.setPlotFrequencies(FrequencyDictionary.getPlotFrequencies());
         this.values = values;
-        this.N1=N1;
-        this.l1=l1;
-        this.N2=N2;
-        this.l2=l2;
-        //TODO 矩法计算参数
-        //TODO 计算经验点距 两个重现期
+        this.values.sort(Collections.reverseOrder());
+        frequencies=new ArrayList<>();
+        calculateParameter();
+        for(int i=1;i<=a1;i++){
+            frequencies.add(new BigDecimal((i/(N1+1.0))*100)
+                    .setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        }
+        double tmp=frequencies.get(a1-1)/100;
+        for(int i=l1+1;i<=a2;i++){
+            frequencies.add(new BigDecimal((tmp+(1-tmp)* i/(N2+1.0))*100)
+                    .setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        }
+        tmp=frequencies.get(frequencies.size()-1)/100;
+        for(int i=l2+1;i<=values.size()-a1-a2;i++){
+            frequencies.add(new BigDecimal((tmp+(1-tmp)* i/(values.size()-a1-a2+1.0))*100)
+                    .setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        }
     }
 
     /**
@@ -207,15 +179,24 @@ public class PIIIFrequencyAnalysis {
     }
 
     /**
-     * get simulate data with specific parameter
-     * @return list  Map<String,List<Double>>  key:1.frequencies 2.values
-     * @see FrequencyDictionary
+     * calculate initial parameter by Moment method
      */
-    public Map<String,List<Double>> listSimulatePoints(){
-        plotFrequencies=FrequencyDictionary.getPlotFrequencies();
-        plotValues=FrequencyDictionary.listSimulatePoints(plotFrequencies,ex,cs,cv);
-        map.put("frequencies",plotFrequencies);
-        map.put("values",plotFrequencies);
-        return map;
+    private void calculateParameter(){
+        double sum=0;
+        //(ki-1)^2
+        double sumk2=0;
+        //(ki-1)^3
+        double sumk3=0;
+        for (Double value1 : values) {
+            sum += value1;
+        }
+        sum=sum/values.size();
+        for (Double value : values) {
+            sumk2 += Math.pow(value / sum - 1, 2);
+            sumk3 += Math.pow(value / sum - 1, 3);
+        }
+        this.setEx(new BigDecimal(sum).setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        this.setCv(new BigDecimal(Math.sqrt(sumk2/(values.size()-1))).setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
+        this.setCs(new BigDecimal(sumk3/((values.size()-3)*Math.pow(this.getCv(),3))).setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue());
     }
 }
